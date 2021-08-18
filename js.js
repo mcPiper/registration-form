71efdfafee64c749aa76a1c8892429aa676d6bac
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-08-18 10:25:43 +0300

    init: upload JS

---
+++ js.js
@@ -0,0 +1,11 @@
+
+// Получить модель
+var modal = document.getElementById('id01');
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+ 
